3de32696825ed98e29be728d094da21a6d06f164
gilgasa <gilgasan@gmail.com>
2022-12-13 11:05:31 -0500

    sonar

--- src/sonarprueba/SonarPrueba.java
+++ src/sonarprueba/SonarPrueba.java
@@ -17,6 +17,7 @@ public class SonarPrueba {
     public static void main(String[] args) {
         System.out.println("Hola Mundo ");
         System.out.println("Hola Mundo 2");
+        System.out.println("Hola Mundo 3");
     }
     
 }
